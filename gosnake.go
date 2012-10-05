b5b04c2382d2694faf5a69a22ac5f193fcc2958e
Sam Nardoni <samnardoni@gmail.com>
2012-10-05 22:57:56 +0100

    add gosnake.go

---
+++ gosnake.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"fmt"
+	"github.com/banthar/gl"
+	"github.com/banthar/glu"
+	"github.com/jteeuwen/glfw"
+	"os"
+)
+
+type Counter struct {
+	hit, current int
+}
+
+func (c *Counter) Tick() bool {
+	c.current++
+	if c.current%c.hit == 0 {
+		return true
+	}
+	return false
+}
+
+type Coord struct {
+	x, y int
+}
+
+const (
+	Title  = "Snake"
+	Width  = 480
+	Height = 480
+)
+
+const (
+	DirUp = iota
+	DirDown
+	DirLeft
+	DirRight
+)
+
+var (
+	snake   []Coord
+	dir     int     = DirUp
+	running bool    = true
+	counter Counter = Counter{hit: 6, current: 0}
+)
+
+func main() {
+	var err error
+
+	if err = glfw.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "[e] %v\n", err)
+		return
+	}
+	defer glfw.Terminate()
+
+	if err = glfw.OpenWindow(Width, Height, 8, 8, 8, 8, 0, 8, glfw.Windowed); err != nil {
+		fmt.Fprintf(os.Stderr, "[e] %v\n", err)
+		return
+	}
+	defer glfw.CloseWindow()
+
+	glfw.SetSwapInterval(1)
+	glfw.SetWindowTitle(Title)
+	glfw.SetWindowSizeCallback(onResize)
+	glfw.SetKeyCallback(onKey)
+
+	initGL()
+	initSnake()
+
+	for running && glfw.WindowParam(glfw.Opened) == 1 {
+		update()
+		drawScene()
+	}
+}
+
+func onResize(w, h int) {
+	if h == 0 {
+		h = 1
+	}
+
+	gl.Viewport(0, 0, w, h)
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadIdentity()
+	glu.Perspective(45.0, float64(w)/float64(h), 0.1, 100.0)
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.LoadIdentity()
+}
+
+func onKey(key, state int) {
+	// TODO Magic 1?
+	if state != 1 {
+		return
+	}
+
+	switch key {
+	case glfw.KeyEsc:
+		running = false
+	case glfw.KeyUp:
+		dir = DirUp
+	case glfw.KeyDown:
+		dir = DirDown
+	case glfw.KeyLeft:
+		dir = DirLeft
+	case glfw.KeyRight:
+		dir = DirRight
+	}
+}
+
+func initGL() {
+	gl.ShadeModel(gl.SMOOTH)
+	gl.ClearColor(0, 0, 0, 0)
+	gl.ClearDepth(1)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LEQUAL)
+	gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
+}
+
+func initSnake() {
+	snake = append(snake, Coord{x: 1, y: 2})
+	snake = append(snake, Coord{x: 2, y: 2})
+	snake = append(snake, Coord{x: 2, y: 3})
+	snake = append(snake, Coord{x: 2, y: 4})
+	snake = append(snake, Coord{x: 3, y: 4})
+	snake = append(snake, Coord{x: 4, y: 4})
+	dir = DirUp
+}
+
+func update() {
+
+	if hit := counter.Tick(); !hit {
+		return
+	}
+
+	last := snake[len(snake)-1]
+
+	switch dir {
+	case DirUp:
+		snake = append(snake, Coord{x: last.x, y: last.y + 1})
+	case DirDown:
+		snake = append(snake, Coord{x: last.x, y: last.y - 1})
+	case DirLeft:
+		snake = append(snake, Coord{x: last.x - 1, y: last.y})
+	case DirRight:
+		snake = append(snake, Coord{x: last.x + 1, y: last.y})
+	}
+
+	snake = snake[1:]
+}
+
+func drawScene() {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	for _, coord := range snake {
+		gl.LoadIdentity()
+		gl.Scalef(0.05, 0.05, 1)
+		gl.Translatef(float32(coord.x)-5, float32(coord.y)-5, -1)
+
+		gl.Begin(gl.QUADS)
+		gl.Color3f(float32(coord.y)/10, float32(coord.x)/10, 0.5)
+		gl.Vertex3f(0, 1, 0)
+		gl.Vertex3f(1, 1, 0)
+		gl.Color3f(float32(coord.y)/10-0.2, float32(coord.x)/10-0.2, 0.3)
+		gl.Vertex3f(1, 0, 0)
+		gl.Vertex3f(0, 0, 0)
+		gl.End()
+	}
+
+	glfw.SwapBuffers()
+}
